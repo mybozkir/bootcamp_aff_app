feb6d6c58a09367926c66f744967bd07966aa537
Mert Yasin Bozkir <mertbozkirofficial@gmail.com>
2022-05-09 17:13:15 +0300

    categories listesi tamamlandı

---
+++ lib/screens/anxiety_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AnxietyScreen extends StatelessWidget {
+  const AnxietyScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Anxiety Screen"),
+    );
+  }
+}
---
+++ lib/screens/money_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MoneyScreen extends StatelessWidget {
+  const MoneyScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Money Screen"),
+    );
+  }
+}
